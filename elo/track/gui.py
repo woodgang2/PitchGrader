4e9f207d437945dc5ce4ba181e2289c9ec630d4f
gang <wsg9mf@virginia.edu>
2024-03-28 20:33:08 -0400

    Persist selected teams

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -438,8 +438,9 @@ else:
     if (st.session_state.team_name_update != '') & (st.session_state['team_name'] == ''):
         st.session_state['team_name'] = st.session_state.team_name_update
     # st.success (st.session_state['player_name'])
-    default_index = options_teams.index(st.session_state['team_name']) if st.session_state['team_name'] in options_teams else 0
     st.session_state['team_name'] = st.session_state['team_name']
+    default_index = options_teams.index(st.session_state['team_name']) if st.session_state['team_name'] in options_teams else 0
+    # st.session_state['team_name'] = st.session_state['team_name']
     team_name = st.selectbox('Team ID (UVA is VIR_CAV)', options=options_teams, index= default_index, key='team_name')
     # st.success ('n2', team_name, 'h')
     st.success (team_name)
