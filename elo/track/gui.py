31b6e49806deedb3fa6528e9fd8a41d987c69de0
gang <wsg9mf@virginia.edu>
2024-04-14 04:12:50 -0400

    Added functionality to view changes between seasons

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -335,7 +335,7 @@ if not st.session_state.team_flag:
                 stuff_df2 = stuff_df2.round(0)
                 # st.dataframe (stuff_df2)
                 merged_df = stuff_df1.merge(stuff_df2, on='Pitcher', how='left', suffixes=('_df2', '_df1'))
-                st.dataframe (merged_df)
+                # st.dataframe (merged_df)
                 # st.dataframe (merged_df)
                 def calculate_and_format(row, col):
                     original = row[f"{col}_df2"]
