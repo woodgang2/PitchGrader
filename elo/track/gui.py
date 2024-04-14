afe7321f4dce18ecfc52766a45e1407473d3012a
gang <wsg9mf@virginia.edu>
2024-04-14 03:13:44 -0400

    Testing2

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -321,12 +321,12 @@ if not st.session_state.team_flag:
                 location_df = location_df.rename(columns={'Overall': 'Command'})
                 # st.dataframe (location_df)
                 stuff_df2 = driver2.retrieve_stuff (name)
-                st.dataframe (stuff_df2)
+                # st.dataframe (stuff_df2)
                 stuff_df2 = stuff_df2.merge (location_df, on = 'Pitcher')
                 stuff_df2 = stuff_df2.round(0)
-                st.dataframe (stuff_df2)
+                # st.dataframe (stuff_df2)
                 merged_df = stuff_df.merge(stuff_df2, on='Pitcher', how='left', suffixes=('_df2', '_df1'))
-                st.dataframe (merged_df)
+                # st.dataframe (merged_df)
                 def calculate_and_format(row, col):
                     original = row[f"{col}_df2"]
                     if pd.isna(row[f"{col}_df1"]):
