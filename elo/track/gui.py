bf7fdafd5c306c3433b92b36b1d10961a4f06a7a
gang <wsg9mf@virginia.edu>
2024-04-24 03:14:26 -0400

    Implementing MC for stuff change4

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -696,7 +696,7 @@ if not st.session_state.team_flag:
 
             for index, row in prob_df.iterrows():
                 modified_prob_MC_df = calculate_mahalanobis(row.to_frame().T, prob_MC_df, ['RelSpeed', 'InducedVertBreak', 'HorzBreak', 'VAA', 'SpinRate', 'SpinEfficiency', 'AxisDifference', 'RelHeight', "RelSide", 'Extension', 'VertRelAngle', 'HorzRelAngle'])
-                sampled_indices = sample_performance(modified_prob_MC_df, 100000)
+                sampled_indices = sample_performance(modified_prob_MC_df, 1000000)
                 simulation_results = monte_carlo_simulation(modified_prob_MC_df, sampled_indices, performance_metrics)
                 simulation_results_per_row.append(simulation_results['Stuff_diff']['75th_percentile'])
                 simulation_results_per_row_std.append(simulation_results['Stuff_diff']['std'])
