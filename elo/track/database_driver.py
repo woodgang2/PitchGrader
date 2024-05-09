14b4822bfa9e0750ce3d7f20fe4344572cfb5533
gang <wsg9mf@virginia.edu>
2024-05-09 02:52:24 -0400

    Added game log

--- elo/track/database_driver.py
+++ elo/track/database_driver.py
@@ -530,7 +530,7 @@ class DatabaseDriver:
         self.write_data()
 
     def retrieve_game_logs (self, player):
-        db_filename = os.path.join(self.current_dir, f'{self.year}game_logs.parquet')
+        db_filename = os.path.join(self.current_dir, f'game_logs.parquet')
         df = pd.read_parquet(db_filename)
         df = df [df['Pitcher'] == player]
         return df
