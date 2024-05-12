4cd00ace564810073111d33ff74cb254c4358487
gang <wsg9mf@virginia.edu>
2024-05-12 05:15:07 -0400

    Team list - colors

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -997,6 +997,11 @@ else:
                 eligible_teams['Stuff Rank'] = eligible_teams['Stuff'].rank(ascending=False, method='min')
                 eligible_teams['Command Rank'] = eligible_teams['Command'].rank(ascending=False, method='min')
                 weighted_averages = weighted_averages.join(eligible_teams[['Stuff Rank', 'Command Rank']], how='left')
+                order = ['PitchCount', 'Command', 'Command Rank', 'Stuff', 'Stuff Rank', 'FF', 'SI', 'FC', 'SL', 'CU', 'FS', 'CH']
+                weighted_averages = weighted_averages [order]
+                colored_columns = ['Command', 'Stuff', 'FF', 'SI', 'FC', 'SL', 'CU', 'FS', 'CH']
+                if not show_changes:
+                    stuff_df = stuff_df.style.applymap(color_values, subset = colored_columns).format("{:.0f}")
                 st.dataframe (weighted_averages)
             colored_columns = ['Command', 'Stuff', 'FF', 'SI', 'FC', 'SL', 'CU', 'FS', 'CH']
             colored_columns = [col for col in colored_columns if col in stuff_df.columns]
