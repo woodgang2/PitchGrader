c83ad795134229a4f80db150bb1695d7ec2920e5
gang <wsg9mf@virginia.edu>
2024-05-12 21:53:39 -0400

    Test

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -1011,7 +1011,7 @@ else:
             colored_columns = ['Command', 'Stuff', 'FF', 'SI', 'FC', 'SL', 'CU', 'FS', 'CH']
             colored_columns = [col for col in colored_columns if col in stuff_df.columns]
             if not show_changes and show_color:
-                stuff_df = stuff_df.style.applymap(color_values, subset = colored_columns)#.format("{:.0f}", subset = colored_columns)
+                stuff_df = stuff_df.style.applymap(color_values, subset = colored_columns).format("{:.0f}", subset = colored_columns)
                 stuff_df = stuff_df.format("{:.2f}", subset = ['Fastball%'])
             container = st.container()
             container.markdown("<div margin-left: auto, margin-right: auto>", unsafe_allow_html=True)
