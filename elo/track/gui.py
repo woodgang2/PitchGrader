7aa304678e0571dd20859f6c37b4108326c6e625
gang <wsg9mf@virginia.edu>
2024-05-16 05:22:33 -0400

    Fixed bug - history not loading for combined

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -679,6 +679,8 @@ if not st.session_state.team_flag:
             input_df = st.data_editor(prob_df)
             st.write ("History")
             stuff_history_df = driver.retrieve_stuff_history(name.translate(str.maketrans('', '', '0123456789')))
+            st.success (name.translate(str.maketrans('', '', '0123456789')))
+            st.dataframe (stuff_history_df)
             location_history_df = driver.retrieve_location_history(name.translate(str.maketrans('', '', '0123456789')))
             location_history_df = location_history_df [['Pitcher', 'Overall', 'Year']]
             location_history_df = location_history_df.rename(columns={'Overall': 'Command'})
