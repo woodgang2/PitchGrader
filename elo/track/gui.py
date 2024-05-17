574a97b546fdabe772a236c270d03a831a9c41a5
gang <wsg9mf@virginia.edu>
2024-05-17 06:50:34 -0400

    Added info panel

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -154,7 +154,6 @@ st.title('PitchGrader')
 # col1, col2, col3 = st.columns([4, 2, 4])
 # st.write('Database last updated 5/7/2024')
 col1, col2, col3 = st.columns([14, 6, 6])
-col1, col2, col3 = st.columns([14, 6])
 with col1:
     st.caption ('Stuff, Location, and Swing Mechanics models for collegiate players')
 with col2:
@@ -163,6 +162,7 @@ with col2:
 with col3:
     if st.button("⚙️"):
         settings_dialog()
+col1, col2, col3 = st.columns([14, 6])
 with col1:
     # st.caption ('Stuff, Location, and Swing Mechanics models for collegiate players')
     st.write('Database last updated: 5/7/2024')
