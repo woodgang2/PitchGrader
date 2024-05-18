630d0fadfc46792b53415e7fdb318237a7f979c7
gang <wsg9mf@virginia.edu>
2024-05-18 01:47:14 -0400

    Tabs

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -298,9 +298,9 @@ with col3:
 # <hr style='margin-top: 0.2em; margin-bottom: 1.2em; height: 0.12em; border: none; background-color: #31333F;'>
 # """, unsafe_allow_html=True)
 container_a = st.container ()
-tab1, tab2 = st.tabs(["Player", "Team"])
+# tab1, tab2 = st.tabs(["Player", "Team"])
 year_selected = st.selectbox ("Year", options = ['Combined', 2024, 2023], index = 1, key = 'year')
-
+tab1, tab2 = st.tabs(["Player", "Team"])
 year = year_selected
 if (year_selected == 'Combined'):
     year = ''
