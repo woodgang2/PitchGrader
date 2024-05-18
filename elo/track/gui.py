82ecfa9a0ca1b039ca3b1e9e675df0e0bd0efc9c
gang <wsg9mf@virginia.edu>
2024-05-17 23:54:33 -0400

    Width changes

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -16,7 +16,6 @@ st.set_page_config(layout="wide")
 def set_page_container_style(
         max_width: int = 1100, max_width_100_percent: bool = False,
         padding_top: int = 1, padding_right: int = 10, padding_left: int = 1, padding_bottom: int = 10,
-        color: str = COLOR, background_color: str = BACKGROUND_COLOR,
 ):
     if max_width_100_percent:
         max_width_str = f'max-width: 100%;'
@@ -34,10 +33,6 @@ def set_page_container_style(
                     padding-right: {padding_right}rem;
                     padding-left: {padding_left}rem;
                     padding-bottom: {padding_bottom}rem;
-                }}
-                .reportview-container .main {{
-                    color: {color};
-                    background-color: {background_color};
                 }}
             </style>
             ''',
