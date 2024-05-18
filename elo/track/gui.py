5ea94ebde869a9bcf52e7d9587908dff82933a5e
gang <wsg9mf@virginia.edu>
2024-05-17 23:59:42 -0400

    Width changes

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -24,7 +24,7 @@ st.markdown(
                 }}
 
         </style>""".format(
-        padding_top=1, padding_bottom=1, padding_right = 10, padding_left = 10
+        padding_top=1, padding_bottom=1, padding_right = 15, padding_left = 15
     ),
     unsafe_allow_html=True,
 )
