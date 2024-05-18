7711c236cd0223c5d3ab741296e95b090e7150b1
gang <wsg9mf@virginia.edu>
2024-05-17 23:55:31 -0400

    Width changes

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -13,36 +13,17 @@ import stuff_plus
 import pandas as pd
 import streamlit as st
 st.set_page_config(layout="wide")
-def set_page_container_style(
-        max_width: int = 1100, max_width_100_percent: bool = False,
-        padding_top: int = 1, padding_right: int = 10, padding_left: int = 1, padding_bottom: int = 10,
-):
-    if max_width_100_percent:
-        max_width_str = f'max-width: 100%;'
-    else:
-        max_width_str = f'max-width: {max_width}px;'
-    st.markdown(
-        f'''
+st.markdown(
+    f'''
             <style>
-                .reportview-container .css-1lcbmhc .css-1outpf7 {{
-                    padding-top: 35px;
+                .reportview-container .sidebar-content {{
+                    padding-top: {1}rem;
                 }}
                 .reportview-container .main .block-container {{
-                    {max_width_str}
-                    padding-top: {padding_top}rem;
-                    padding-right: {padding_right}rem;
-                    padding-left: {padding_left}rem;
-                    padding-bottom: {padding_bottom}rem;
+                    padding-top: {100}rem;
                 }}
             </style>
-            ''',
-        unsafe_allow_html=True,
-    )
-
-set_page_container_style(
-    max_width = 1100, max_width_100_percent = True,
-    padding_top = 0, padding_right = 10, padding_left = 5, padding_bottom = 10
-)
+            ''',unsafe_allow_html=True)
 
 # st.write("""
 # # My first app
