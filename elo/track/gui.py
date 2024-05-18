080f7fe2cf257eb46b02b6671d33d004ef84091b
gang <wsg9mf@virginia.edu>
2024-05-18 03:41:12 -0400

    Debugging2

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -421,25 +421,25 @@ with tab1:
     # first_name = st.text_input('First Name', '', placeholder='First name', key='first_name')
     # last_name = st.text_input('Last Name', '', placeholder='Last name', key='last_name')
     # team_name = st.text_input('Team Name', '', placeholder='Team name', key='team_name')
-    # st.success (st.session_state['player_name'])
+    st.success (st.session_state['player_name'])
 
     if (st.session_state.player_name_update != '') & (st.session_state['player_name'] == ''):
         st.session_state['player_name'] = st.session_state.player_name_update
-    # st.success (st.session_state['player_name'])
+    st.success (st.session_state['player_name'])
     def pick_random ():
         random_option = random.choice(options)
         st.session_state['player_name'] = random_option
     if 'player_name' not in st.session_state:
         # st.success ("not in session state")
         st.session_state['player_name'] = options[0]
-    # st.success (st.session_state['player_name'])
+    st.success (st.session_state['player_name'])
     # st.dataframe (options)
-    # st.error (options.index(st.session_state['player_name']))
+    st.error (options.index(st.session_state['player_name']))
     default_index = options.index(st.session_state['player_name']) if st.session_state['player_name'] in options else 0
     if default_index == 0:
         # st.success ("default index = 0")
         st.session_state['player_name'] = ''
-    # st.success (st.session_state['player_name'])
+    st.success (st.session_state['player_name'])
     selected_name = st.selectbox('Name', options=options, index=default_index, key='player_name')# index=default_index, key='player_name')
     st.session_state.player_name_update = selected_name
     team_name = ''
