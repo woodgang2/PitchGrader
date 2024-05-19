d92dc8a72bd90cdb68dc659f3a564fba30e0678e
gang <wsg9mf@virginia.edu>
2024-05-19 00:22:50 -0400

    Debugging

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -443,7 +443,9 @@ with tab1:
         # st.success ("default index = 0")
         # st.session_state['player_name'] = ''
     st.success (st.session_state['player_name'])
-    st.session_state.player_name_update = st.selectbox('Name', options=options, index=default_index, key='player_name')# index=default_index, key='player_name')
+    def update_player_name():
+        st.session_state['player_name'] = st.session_state.player_name_update
+    st.session_state.player_name_update = st.selectbox('Name', options=options, index=default_index, key='player_name', on_change=update_player_name)# index=default_index, key='player_name')
     # st.session_state.player_name_update = selected_name
     team_name = ''
     # When both names have been entered, display the full name
