a04ac78bbca49173488f7a8fce07739fc0a4dc9c
gang <wsg9mf@virginia.edu>
2024-05-19 00:14:22 -0400

    Debugging

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -426,6 +426,7 @@ with tab1:
         random_option = random.choice(options)
         st.session_state['player_name'] = random_option
     if (st.session_state.player_name_update != '') & (st.session_state['player_name'] == ''):
+        st.error ("proc")
         st.session_state['player_name'] = st.session_state.player_name_update
     elif st.session_state['player_name'] == '':
         pick_random()
