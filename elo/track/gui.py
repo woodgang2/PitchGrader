cbfd7b8df2cad8bf42f7332eeaaf1533691675d1
gang <wsg9mf@virginia.edu>
2024-05-19 00:37:00 -0400

    Debugging team name

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -1069,18 +1069,19 @@ with tab2:
     # st.success (st.session_state['team_name'])
     # team_name = st.text_input('Team ID (from trackman)', '', placeholder='Team ID (UVA is VIR_CAV) - Enter "All" to see all players', key='team_name')
     # team_name = st.selectbox('Team ID (UVA is VIR_CAV)', options=options_teams, key='team_name')
-    if (st.session_state.team_name_update != '') & (st.session_state['team_name'] == ''):
-        st.session_state['team_name'] = st.session_state.team_name_update
+    # if (st.session_state.team_name_update != '') & (st.session_state['team_name'] == ''):
+    #     st.session_state['team_name'] = st.session_state.team_name_update
     # st.success (st.session_state['player_name'])
-    st.session_state['team_name'] = st.session_state['team_name']
-    default_index = options_teams.index(st.session_state['team_name']) if st.session_state['team_name'] in options_teams else 0
-    if default_index == 0:
-        st.session_state['team_name'] = ''
     # st.session_state['team_name'] = st.session_state['team_name']
-    team_name = st.selectbox('Team ID (UVA is VIR_CAV)', options=options_teams, index= default_index, key='team_name')
+    default_index = options_teams.index(st.session_state['team_name_update']) if st.session_state['team_name_update'] in options_teams else 0
+    # if default_index == 0:
+    #     st.session_state['team_name'] = ''
+    # st.session_state['team_name'] = st.session_state['team_name']
+    st.session_state.team_name_update  = st.selectbox('Team ID (UVA is VIR_CAV)', options=options_teams, index= default_index)#, key='team_name')
+    team_name = st.session_state.team_name_update
     # st.success ('n2', team_name, 'h')
     # st.success (team_name)
-    st.session_state.team_name_update = team_name
+    # st.session_state.team_name_update = team_name
     # st.success ('n1')
     min_pitch = st.text_input('Minimum Pitch Count', '', placeholder='Pitch Count', key='min_pitch')
     st.empty ()
