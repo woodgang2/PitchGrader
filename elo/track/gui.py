ef38cf45814f394dcc8e9d8d568199e4289f1e3c
gang <wsg9mf@virginia.edu>
2024-05-19 18:41:20 -0400

    Debugging name year flag

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -425,7 +425,8 @@ with tab1:
     def pick_random ():
         random_option = random.choice(options)
         st.session_state['player_name'] = random_option
-    pick_random ()
+    if (st.session_state['player_name'] == ''):
+        pick_random ()
     # if (st.session_state.player_name_update != '') & (st.session_state['player_name'] == ''):
     #     st.error ("proc")
     #     st.session_state['player_name'] = st.session_state.player_name_update
