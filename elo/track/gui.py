2f16e7f513588e651df695412895be4dd6129493
gang <wsg9mf@virginia.edu>
2024-05-21 01:18:29 -0400

    Formatting

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -900,6 +900,7 @@ with tab1:
                 role_series = pd.Series(roles, name='Role')
                 stuff_history_df = stuff_history_df.insert(2, 'Role', role_series)
                 st.empty ()
+                st.dataframe (stuff_history_df)
                 stuff_history_df = stuff_history_df.style.applymap(color_values, subset = colored_columns).format("{:,.0f}", subset = colored_columns + ['PitchCount'])
                 st.empty ()
                 st.dataframe (stuff_history_df)
